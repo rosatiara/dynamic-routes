f572b58b2f6fa666925f887fb071189c46abbe13
rosa <rosatiaragaluh@gmail.com>
2022-05-06 21:57:40 +0700

    feat: implement get static paths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,17 @@
 import Layout from '../../components/layout';
-
-export default function Post() {
-  return <Layout>...</Layout>;
-}
-
+import { getAllPostIds } from '../../lib/posts';
 
 export async function getStaticPaths() {
   // return a list of possible value for id
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default function Post() {
+  return <Layout>...</Layout>;
 }
 
 export async function getStaticProps ({}) {
