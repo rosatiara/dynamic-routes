a779142b614f69b59bbd3a270e8047e592bb88b3
rosa <rosatiaragaluh@gmail.com>
2022-05-06 21:56:15 +0700

    feat: add functions

---
+++ pages/posts/[id].js
@@ -0,0 +1,14 @@
+import Layout from '../../components/layout';
+
+export default function Post() {
+  return <Layout>...</Layout>;
+}
+
+
+export async function getStaticPaths() {
+  // return a list of possible value for id
+}
+
+export async function getStaticProps ({}) {
+  // fetch necessary data for the post using params.id
+}
